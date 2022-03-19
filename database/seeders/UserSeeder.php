cdc3e963d4c0e57154cbdc6b56fb8e12e03be870
nandaputrakarrel <nandaputrakarrel@gmail.com>
2022-03-20 01:12:52 +0700

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'testing',
+            'email' => 'testing@gmail.com',
+            'gender' => 'male',
+            'birthdate' => '2000-01-01',
+            'password' => '$2y$10$Cp67YzXZti91I29gzEGYXuIYPIUDnfjIQ3c8liSKAbznrXNiLveNS' // password
+        ]);
+    }
+}
